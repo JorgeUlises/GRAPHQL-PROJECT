0664fb2d871186aa6b5114203be0894b03561914
JorgeUlises <opti.jugr@gmail.com>
2022-06-27 23:25:38 -0500

    inicio de repositorio

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const graphqlHTTP = require('express-graphql').graphqlHTTP;
+
+const schema = require('./schema/schema');
+
+const app= express();
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true,
+    schema: schema
+}))
+
+app.listen(4000, ()=>{
+    console.log('listening in port 4000');
+})
---
+++ schema/schema.js
@@ -0,0 +1,70 @@
+const graphql = require('graphql');
+var _ = require('lodash');
+
+//dummy data
+var usersData = [
+    {id: '1', name: 'Juan', age:25},
+    {id: '2', name: 'Maria', age:20},
+    {id: '3', name: 'Luis', age:22},
+    {id: '4', name: 'Pedro', age:35},
+]
+
+const {
+    GraphQLObjectType,
+    GraphQLID,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema
+} = graphql
+
+
+//Create types
+const UserType = new graphql.GraphQLObjectType({
+    name: 'User',
+    description: 'Documentation for user ...',
+    fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: graphql.GraphQLString},
+        age: {type: GraphQLInt}
+    })
+});
+
+// RouteQuery
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    description: 'Description',
+    fields: {
+        user: {
+            type: UserType,
+            args: {id: {type: GraphQLString}},
+
+            resolve(parent, args){
+                // let user = {
+                //     id: '334',
+                //     age: 34,
+                //     name: 'Paulo'
+                // }
+                // return user;
+
+                return _.find(usersData, {id: args.id})
+                // we resolve with data
+                //get and return data from a datasource
+            }
+        }
+    }
+});
+
+/*
+{
+    user(id: "1"){
+        name
+        age
+        id
+    }
+}
+
+*/
+
+module.exports = new graphql.GraphQLSchema({
+    query: RootQuery
+}) 
